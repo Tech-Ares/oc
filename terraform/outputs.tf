b1103bb19da9e66f1cfe233a05c3086acf52570d
Ares <85914637+Tech-Ares@users.noreply.github.com>
2025-07-18 01:05:25 +0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,26 @@
-output "task_definition_arn" {
-  value = aws_ecs_task_definition.app_task.arn
+# outputs.tf
+
+output "ecs_cluster_name" {
+  description = "ECS 叢集名稱"
+  value       = aws_ecs_cluster.my_ec2_cluster.name
 }
 
-output "service_name" {
-  value = aws_ecs_service.app_service.name
+output "ecs_service_name" {
+  description = "ECS 服務名稱"
+  value       = aws_ecs_service.app_service.name
+}
+
+output "ecr_repository_url" {
+  description = "ECR 儲存庫 URL"
+  value       = aws_ecr_repository.app_ecr.repository_url
+}
+
+output "ecs_task_definition_arn" {
+  description = "ECS 任務定義 ARN"
+  value       = aws_ecs_task_definition.app_task.arn
+}
+
+output "ecs_container_instance_id" {
+  description = "ECS 容器實例 ID"
+  value       = aws_instance.ecs_container_instance.id
 }
